d7fac4ff9b2cc199e22fdd3f9a5e853bc9291d67
BT_1N3_28@bktech.local <BT_1N3_28@bktech.local>
2019-01-02 08:40:53 -0500

    Initial commit

---
+++ src/com/company/BubbleSort.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class BubbleSort {
+    public static void bubbleSort(int arr[]){
+        int i = 0;
+        int j = 1;
+        while(i < array.length())
+        if (arr[i] > arr[j]) {
+            int x = arr[i];
+            arr[i] = arr[j];
+            arr[j] = x;
+            i++;
+            j++;
+        }
+    }
+}
