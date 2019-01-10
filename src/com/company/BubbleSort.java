bced8183ebafe4782112b7f49910641bac1f8422
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2019-01-10 08:41:03 -0500

    Update BubbleSort.java

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -1,16 +1,36 @@
 package com.company;
 
 public class BubbleSort {
-    public static void bubbleSort(int arr[]){
-        int i = 0;
-        int j = 1;
-        while(i < array.length())
-        if (arr[i] > arr[j]) {
-            int x = arr[i];
-            arr[i] = arr[j];
-            arr[j] = x;
-            i++;
-            j++;
+    public static void bubbleSort(int[] Arr)
+    {
+        while (checkList(Arr))
+        {
+            for (int i = 0; i < Arr.length; i++)
+            {
+                if (Arr[i] > Arr[i + 1])
+                {
+                    swap(Arr, i+0,i + 1);
+                }
+            }
         }
+    }
+
+    public static void swap(int[]Arr, int i, int j)
+    {
+        int temp = Arr[i];
+        Arr[i] = Arr[j];
+        Arr[j] = temp;
+    }
+
+    public static boolean checkList(int[] Arr)
+    {
+        for (int x=0; x <Arr.length; x++)
+        {
+            if (Arr[x] > Arr[x+1])
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
